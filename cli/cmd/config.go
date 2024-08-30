b89edc95f617ef7372d835eeacb8219b6d39a095
Johannes7k75 <Jonashebenstreit1@gmail.com>
2024-08-30 02:43:50 +0200

    updates

--- cli/cmd/config.go
+++ cli/cmd/config.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
@@ -12,12 +14,18 @@ var configCmd = &cobra.Command{
 	Short: "Manage configuration settings",
 }
 
+type ConfigUpdate struct {
+	apiUrl      string
+	accountsUrl string
+}
+
 // Subcommand for 'config show'
 var showCmd = &cobra.Command{
 	Use:   "show",
 	Short: "Show configuration settings",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("host:", viper.GetString("host"))
+		fmt.Println("api:", viper.GetString("endpoint.api"))
+		fmt.Println("accounts:", viper.GetString("endpoint.accounts"))
 	},
 }
 
@@ -26,31 +34,55 @@ var updateCmd = &cobra.Command{
 	Use:   "update",
 	Short: "Update a configuration setting",
 	Run: func(cmd *cobra.Command, args []string) {
-		viper.Set("host", host)
-		err := viper.WriteConfig()
-		if err != nil {
-			fmt.Println("Error updating 'host' configuration:", err)
+		recoverWithLog()
+		var flags = &ConfigUpdate{}
+
+		cmd.Flags().VisitAll(func(f *pflag.Flag) {
+			if f.Name == "apiUrl" {
+				flags.apiUrl = f.Value.String()
+			}
+			if f.Name == "accountsUrl" {
+				flags.accountsUrl = f.Value.String()
+			}
+		})
+
+		if flags.apiUrl == "" && flags.accountsUrl == "" {
+			fmt.Println("apiUrl or accountsUrl is required")
+			fmt.Println(cmd.Flags().FlagUsages())
 			return
 		}
-		fmt.Println("Updating 'host' configuration:", host)
+
+		if flags.apiUrl != "" {
+			viper.Set("endpoint.api", flags.apiUrl)
+			fmt.Println("Updating 'api' configuration:", flags.apiUrl)
+		}
+		if flags.accountsUrl != "" {
+			viper.Set("endpoint.accounts", flags.accountsUrl)
+			fmt.Println("Updating 'accounts' configuration:", flags.accountsUrl)
+		}
+
+		err := viper.WriteConfig()
+		if err != nil {
+			fmt.Println("Error updating configuration:", err)
+			return
+		}
 	},
 }
-
-// Flag to specify the 'host' configuration value
-var host string
 
 func init() {
 	// Set up Viper configuration
 	// Set a default value for 'host' configuration
-	viper.SetDefault("host", "https://api.ente.io")
+	viper.SafeWriteConfigAs("/cli-data/config.yaml")
+	viper.SetDefault("endpoint.api", "https://api.ente.io")
+	viper.SetDefault("endpoint.accounts", "https://accounts.ente.io")
 
 	// Add 'config' subcommands to the root command
-	//rootCmd.AddCommand(configCmd)
+	rootCmd.AddCommand(configCmd)
 
 	// Add flags to the 'config store' and 'config update' subcommands
-	updateCmd.Flags().StringVarP(&host, "host", "H", viper.GetString("host"), "Update the 'host' configuration")
+	updateCmd.Flags().StringP("apiUrl", "a", "", "Update the 'api' configuration")
+	updateCmd.Flags().StringP("accountsUrl", "A", "", "Update the 'accounts' configuration")
 	// Mark 'host' flag as required for the 'update' command
-	updateCmd.MarkFlagRequired("host")
 
 	// Add 'config' subcommands to the 'config' command
 	configCmd.AddCommand(showCmd, updateCmd)
